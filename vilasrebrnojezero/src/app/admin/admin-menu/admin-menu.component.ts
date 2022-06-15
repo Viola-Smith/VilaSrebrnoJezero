61b863fac5aa89d2e42f16229372557ff5345a50
Milica Nikolić <milicanikolica97@gmail.com>
2022-06-15 23:25:54 +0200

    Mobile admin menu

--- vilasrebrnojezero/src/app/admin/admin-menu/admin-menu.component.ts
+++ vilasrebrnojezero/src/app/admin/admin-menu/admin-menu.component.ts
@@ -10,11 +10,33 @@ export class AdminMenuComponent implements OnInit {
 
   constructor(private router: Router) { }
 
+  mobileWidth = 1100
+  windowWidth
+
+  showSideMenu = true
+
+  menuClicked = false
+
   ngOnInit() {
+    this.windowWidth = window.innerWidth
+    this.showSideMenu = this.windowWidth >= this.mobileWidth
+    window.addEventListener("resize", ()=>{this.windowWidth = window.innerWidth; this.showSideMenu = this.windowWidth >= this.mobileWidth});
   }
 
   logOut() {
     localStorage.removeItem('admin')
+  }
+
+  showMenu() {
+    this.menuClicked = !this.menuClicked
+    if (this.menuClicked) {
+      document.getElementsByClassName('mobMenu')[0].classList.add('show');
+      document.getElementsByClassName('sidebar-container')[0].classList.add('show');
+    } else {
+      document.getElementsByClassName('mobMenu')[0].classList.remove('show');
+      document.getElementsByClassName('sidebar-container')[0].classList.remove('show');
+    }
+  
   }
 
 }
